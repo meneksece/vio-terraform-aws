d7272ad13b00b94dd2cea35cd5e5e94eec924d98
violetadelamancha <mnksecyln@gmail.com>
2023-12-10 18:28:57 +0300

    create single or multiple roles with attaching access policies (using arns)

---
+++ create_iam_role/data.tf
@@ -0,0 +1,11 @@
+#assume policy
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
